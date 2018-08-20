f2059159c5d314b53b4101472712e0b3ad8d9e25
Kacper096 <kacpermatuszek2@gmail.com>
2018-08-20 10:10:04 +0200

    Wizytowka

--- Wizytowka/js/script.js
+++ Wizytowka/js/script.js
@@ -24,7 +24,7 @@
 
                     }, 1000, function () {
                         $(this).removeClass("hoverOff")
-                    }).addClass("hoverOn");;
+                    }).addClass("hoverOn");
 
             })
         }
