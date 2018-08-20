bbb82b7e677aa6d0e0794908e60d4bdbac3be916
Kacper096 <kacpermatuszek2@gmail.com>
2018-08-20 08:11:52 +0200

    Wizytowka

---
+++ Wizytowka/js/script.js
@@ -0,0 +1,19 @@
+﻿$(document).ready(
+    function () {
+        $(".kafelek1").hover(function () {
+            $(".podpis", this).animate(
+                {
+                    "right": 30,
+                    
+
+                }, 1000).removeClass("hoverOff").addClass("hoverOn");
+           
+        })
+
+        $(".kafelek1").mouseleave(function () {
+            $(".podpis", this).animate(
+                {
+                    "right": 0
+                }, 1000).removeClass("hoverOn").addClass("hoverOff");
+        })
+    })
